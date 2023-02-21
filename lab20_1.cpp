622776a090bcb91e41d2f7e8333cb7559e053704
wayu <i@gmail.com>
2023-02-21 16:53:15 +0700

    not yet

--- lab20_1.cpp
+++ lab20_1.cpp
@@ -5,8 +5,14 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
-
+double overlap(Rect r1,Rect r2){
+	
+	double w=0,h=0;
+	double a=max(r1.x,r2.x);
+	double b=min(r1.x+r1.w,r2.x+r2.w);
+	double j=min(r1.y,r2.y);
+	double k=max(r1.y-r1.h,r2.y-r2.h);
+	if(b>=a) w=b-a;
+	if(j>=k) h=j-k;
+	return w*h;
 }
